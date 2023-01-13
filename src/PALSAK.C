62bdfad11d9a11206198db5f41567a6826b0c311
Benjamin Yang <b98204032@gmail.com>
2023-01-13 16:34:36 +0800

    Testing for Setting IP 7.

--- src/PALSAK.C
+++ src/PALSAK.C
@@ -663,8 +663,10 @@ static int SetPalertNetwork( const uint msec )
 			Delay(1);
 		//}
 	/* */
+		BroadcastAddr.sin_addr.s_addr = inet_addr("192.168.137.99");
+		BroadcastAddr.sin_port = htons(23);
 		sprintf(strbuf, "ip %u.%u.%u.%u\r", addr[0], addr[1], addr[2], addr[3]);
-		while ( BroadcastCommand( "cs 123.123.123.123" ) != NORMAL );
+		while ( BroadcastCommand( "ip 123.123.123.123" ) != NORMAL );
 		Print("668 %s\n\r", MsgBuffer);
 	/* Show 'S. iP.' on the 7-seg led */
 		Show5DigitLedWithDot(1, 0x05);
