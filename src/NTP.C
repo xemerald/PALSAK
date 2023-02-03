6bb866f00c54c6fb3645287a7ad3e431a8f15f21
Benjamin Yang <b98204032@gmail.com>
2023-02-03 23:57:48 +0800

    Testing 32.

--- src/NTP.C
+++ src/NTP.C
@@ -170,7 +170,7 @@ int NTPSend( void )
 	_asm cli
 	tv1 = SoftSysTime;
 	_asm sti
-	Print("\r\nTesting %lld %llu %llu %lld", tv1.tv_usec, frac2usec(0x5bf9573), FRAC_TO_USEC(0x5bf9573UL), USEC_TO_FRAC( tv1.tv_usec ));
+	Print("\r\nTesting %lld %llu %llu %lld", tv1.tv_usec, frac2usec(0x5bf9573), USEC_TO_FRAC( tv1.tv_usec ));
 	*(ulong *)&InternalBuffer[40] = HTONS_FP( tv1.tv_sec + EpochDiff );
 	*(ulong *)&InternalBuffer[44] = HTONS_FP( USEC_TO_FRAC( tv1.tv_usec ) );
 /* Send to the server */
@@ -210,6 +210,7 @@ int NTPRecv( void )
 	/* Get the remote transmit timestamp */
 		tv3.tv_sec  = NTOHS_FP( *(ulong *)&InternalBuffer[40] );
 		tv3.tv_usec = FRAC_TO_USEC( NTOHS_FP( *(ulong *)&InternalBuffer[44] ) );
+		Print("\r\nTesting %llu %llu %llu", tv1.tv_usec, tv2.tv_usec, tv3.tv_usec);
 	/* Calculate the time offset */
 		offset_usec  = (tv2.tv_usec - tv1.tv_usec) + (tv3.tv_usec - tv4.tv_usec);
 		offset_usec += ((tv2.tv_sec - tv1.tv_sec) + (tv3.tv_sec - tv4.tv_sec)) * 1000000;
